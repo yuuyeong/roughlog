92ac4c6f6a471615e348e378dda5eb6f57d3b3c8
may <may@eunjung.vps.phps.kr>
2016-03-28 02:41:23 +0900

    update settings.py set up static and media root

--- Roughlog/Roughlog/settings.py
+++ Roughlog/Roughlog/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = '5dpqwa685-lf4b*qi4%pj(0phu=c6n#=+fs6h78wm%farh&nfk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roughlog.net',]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'Roughlog',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -73,7 +75,6 @@ WSGI_APPLICATION = 'Roughlog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -119,3 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, 'dist', 'static')
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'Roughlog', 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_DIR, 'dist', 'media')
+
+TEMPLATE_DIRS = [
+	os.path.join(BASE_DIR, 'Roughlog','templates'),
+]
+
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+)
