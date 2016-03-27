72b3f3b26b4a3cd57e627062100766858cd3054d
may <may@eunjung.vps.phps.kr>
2016-03-28 03:14:13 +0900

    update urls.py : added media url

--- Roughlog/Roughlog/urls.py
+++ Roughlog/Roughlog/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from Roughlog.views import home
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', home, name="home"),
+] + static(settings.MEDIA_URL, document_ROOT=settings.MEDIA_ROOT)
