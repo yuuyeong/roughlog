512bf283f5cb7f362d492578eab095ee11c263cf
EunJung-Seo <soudg2@gmail.com>
2016-04-07 20:09:03 +0900

    add user profile page

--- Roughlog/users/views/profile.py
+++ Roughlog/users/views/profile.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView
+from django.contrib.auth import get_user_model
+from django.views.generic import DetailView
 
 
-class UserProfilePage(ListView):
+class UserProfilePage(DetailView):
+    model = get_user_model()
     template_name = "users/profile.html"
     slug_field = "nickname"
