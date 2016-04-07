36a6d7facd4a7c0cd802192a4c791755e78bd606
EunJung-Seo <soudg2@gmail.com>
2016-04-07 19:37:17 +0900

    modified article list view: parsing json data

--- Roughlog/articles/views/articles.py
+++ Roughlog/articles/views/articles.py
@@ -1,17 +1,37 @@
+import json
 import requests
 
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.generic import View
 
+from social.apps.django_app.default.models import UserSocialAuth
+
 
 class ArticleListView(View):
-    template_name_name = "articles/list.html"
 
     def get(self, request):
-        user = request.user.social_auth.filter(
-            provider='pocket',
-        ).first()
+        dict_file = self.get_json_file(request.user)
+        lists = [
+            # [ value.get('excerpt'), value.get('resolved_title'), value.get('resolved_url')]
+            value
+            for key, value
+            in dict_file['list'].items()
+        ]
+        # from IPython import embed; embed()
+        return render(
+            request,
+            "articles/list.html",
+            context={
+                'lists': lists,
+            },
+        )
+
+    def get_json_file(self, user_object):
+        user = UserSocialAuth.objects.get(uid=user_object.username)
         url = "https://getpocket.com/v3/get"
 
         if user:
@@ -20,15 +40,12 @@ class ArticleListView(View):
                 data={
                     "consumer_key": settings.SOCIAL_AUTH_POCKET_KEY,
                     "access_token": user.extra_data['access_token'],
+                    "state": "unread",
                 },
                 headers={"X-Accept": "application/json"}
             )
 
-            return HttpResponse(
-                response,
-                content_type="application/json"
+            return json.loads(
+                response.text
             )
-        return HttpResponse(
-                json.dumps([]),
-                content_type="application/json"
-            )
+        return {}
