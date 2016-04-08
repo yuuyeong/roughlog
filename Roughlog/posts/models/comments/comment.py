d87845ad87029385a88a1d6a27db727553e76023
EunJung-Seo <soudg2@gmail.com>
2016-04-08 15:42:02 +0900

    added Comment Model in posts app

---
+++ Roughlog/posts/models/comments/comment.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import models
+
+from posts.models import Post
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+    )
+
+    post = models.ForeignKey(
+        Post,
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
